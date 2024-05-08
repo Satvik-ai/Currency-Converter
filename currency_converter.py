51f2ad0811b8069ff229b9526449862d72817dc2
Satvik <chandrakarsatvik@gmail.com>
2024-05-08 13:00:44 +0530

    First Commit

---
+++ currency_converter.py
@@ -0,0 +1,42 @@
+# importing libraries
+import streamlit as st
+import requests
+from dateutil.parser import parse
+
+# Using ExchangeRate API :- https://www.exchangerate-api.com/
+
+st.title('Currency Converter')
+
+# getting input from user
+col1, col2, col3= st.columns([1,1,1])
+with col1:
+    from_curr = st.selectbox("From",('AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BRL','BSD','BTN','BWP','BYN','BZD','CAD','CDF','CHF','CLP','CNY','COP','CRC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','FOK','GBP','GEL','GGP','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','IMP','INR','IQD','IRR','ISK','JEP','JMD','JOD','JPY','KES','KGS','KHR','KID','KMF','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRU','MUR','MVR','MWK','MXN','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLE','SOS','SRD','SSP','STN','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TVD','TWD','TZS','UAH','UGX','USD','UYU','UZS','VES','VND','VUV','WST','XAF','XCD','XDR','XOF','XPF','YER','ZAR','ZMW','ZWL'))
+with col2:
+    to_curr = st.selectbox("To",('AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BRL','BSD','BTN','BWP','BYN','BZD','CAD','CDF','CHF','CLP','CNY','COP','CRC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','FOK','GBP','GEL','GGP','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','IMP','INR','IQD','IRR','ISK','JEP','JMD','JOD','JPY','KES','KGS','KHR','KID','KMF','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRU','MUR','MVR','MWK','MXN','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLE','SOS','SRD','SSP','STN','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TVD','TWD','TZS','UAH','UGX','USD','UYU','UZS','VES','VND','VUV','WST','XAF','XCD','XDR','XOF','XPF','YER','ZAR','ZMW','ZWL'))
+with col3:
+    amount = st.number_input("Amount")
+
+
+# function to get the exchange rates
+def get_exchange_rates(from_curr):
+    # request the ExchangeRate API and convert the response to Python dict using .json()
+    url = f"https://v6.exchangerate-api.com/v6/a3491705d8ce274e7d484ef3/latest{from_curr}"
+    data = requests.get(url).json()
+    if data["result"] == "success":
+        last_updated_datetime = parse(data["time_last_update_utc"]) # get the last updated datetime
+        exchange_rates = data["rates"] # get the exchange rates
+
+    return last_updated_datetime, exchange_rates
+
+# function to convert the currency
+def convert_currency(from_curr, to_curr, amount):
+    # get the exchange rates
+    last_updated_datetime, exchange_rates = get_exchange_rates(from_curr)
+    # convert by simply getting the target currency exchange rate and multiply by the amount
+    return last_updated_datetime, exchange_rates[to_curr] * amount
+
+last_updated_datetime, exchange_rate = convert_currency(from_curr, to_curr, amount)
+
+# showing the results
+st.markdown(f'### "Last updated datetime:", {last_updated_datetime}')
+st.markdown(f'{amount} {from_curr} = {exchange_rate} {to_curr}')
